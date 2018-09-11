e89f281b1afa947916f8aae455629fca7a8c2427
Tianlikai <1024998526@qq.com>
2018-09-11 15:06:47 +0800

    添加注释

--- js_object/inherit.js
+++ js_object/inherit.js
@@ -6,15 +6,18 @@
  */
 function inherit(p) {
   if (p == null) {
+    // p 为null 直接抛错
     throw TypeError();
   }
   if (Object.create) {
+    // Object.create 直接有继承的作用
     return Object.create(p);
   }
   if (typeof p !== "object" || typeof p !== "function") {
     throw TypeError();
   }
   function F() {}
+  // 让 F 的原型为传入的 p
   F.prototype = p;
   return new F();
 }
