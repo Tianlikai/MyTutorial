6a148c84e0781129d80fcbbb547cec4460a93744
Tianlikai <1024998526@qq.com>
2018-10-15 14:25:40 +0800

    千夫指

--- es/shallowCopy.js
+++ es/shallowCopy.js
@@ -3,3 +3,19 @@
  * 1 Object.assign()
  * 2 一层循环
  */
+
+function shallowCopy(source) {
+  var name;
+  var result = {};
+  for (name in source) {
+    if (source.hasOwnProperty(name)) {
+      result[name] = source[name];
+    }
+  }
+  return result;
+}
+
+let obj = { a: { name: "jason" } };
+let copyOBJ = shallowCopy(obj);
+copyOBJ.a.age = 22;
+console.log(obj.a);
