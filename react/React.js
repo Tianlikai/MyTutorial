9e53f55dcac41362c4e834755d8fdb281ed5a5b1
Tianlikai <1024998526@qq.com>
2018-10-17 18:45:25 +0800

    更改代码组织结构

--- react/React.js
+++ react/React.js
@@ -1,3 +1,9 @@
+import ReactClass from "./ReactClass";
+import ReactElement from "./ReactElement";
+import ReactDOMComponent from "./ReactDOMComponent";
+import ReactDOMTextComponent from "./ReactDOMTextComponent";
+import ReactCompositeComponent from "./ReactCompositeComponent";
+
 /**
  * React 对象
  * @func createElement 创建虚拟元素
@@ -11,7 +17,15 @@ const React = {
    * @param {*} config 元素配置
    * @param {*} children 元素的子元素
    */
-  createElement: function(type, config, children) {},
+  createElement: function(type, config, children) {
+    var props = {};
+    var propName;
+    var config = config || {};
+    // 如果有 key 则作为元素的唯一标示
+    var key = config.key || null;
+
+    return new 
+  },
 
   /**
    * 接收一个React元素，和一个dom节点
--- react/index.js
+++ react/index.js
@@ -1,15 +1,3 @@
 import React from "./React";
-import ReactClass from "./ReactClass";
-import ReactElement from "./ReactElement";
-import ReactDOMComponent from "./ReactDOMComponent";
-import ReactDOMTextComponent from "./ReactDOMTextComponent";
-import ReactCompositeComponent from "./ReactCompositeComponent";
 
-export {
-  React,
-  ReactClass,
-  ReactElement,
-  ReactDOMComponent,
-  ReactDOMTextComponent,
-  ReactCompositeComponent
-};
+export default React;
