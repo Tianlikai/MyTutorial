7222cee8b80354e0df12125a0d98350ded40fb39
Tianlikai <1024998526@qq.com>
2018-10-17 19:03:24 +0800

    添加React API - createElement

--- react/React.js
+++ react/React.js
@@ -24,6 +24,31 @@ const React = {
     // 如果有 key 则作为元素的唯一标示
     var key = config.key || null;
 
+    // 获取config 拥有的元素
+    for (propName in config) {
+      if (config.hasOwnProperty(propName) && propName !== "key") {
+        props[propName] = config[propName];
+      }
+    }
+
+    // 将元素的 children，都挂载到props.children 上
+    // children 用数组存放
+    var childrenLength = arguments.length - 2;
+    if (childrenLength === 1) {
+      props.children = Array.isArray(children) ? children : [children];
+    } else if (childrenLength > 1) {
+      var childArray = [];
+
+      for (var i = 0; i < childrenLength; ++i) {
+        childArray.push(arguments[i + 2]);
+      }
+
+      props.children = childArray;
+    }
+
+    // 此处忽略掉默认属性的处理
+
+    //返回一个React 元素
     return new ReactElement(key, type, props);
   },
 
--- react/ReactDOMComponent.js
+++ react/ReactDOMComponent.js
@@ -1,5 +1,5 @@
 /**
- * componet 类
+ * component 类
  * react 基础标签类型，类似与html中的（'div','span' 等）
  * @param {*} element 基础元素
  */
