fada481fa144e97f2b7f1ed3024398bb226e5d1e
Tianlikai <1024998526@qq.com>
2018-10-18 15:03:14 +0800

    实现 ReactDOMTextComponent-mountComponent 方法

--- react/ReactDOMTextComponent.js
+++ react/ReactDOMTextComponent.js
@@ -8,4 +8,16 @@ function ReactDOMTextComponent(text) {
   this._rootNodeID = null;
 }
 
+/**
+ * component 类 装载方法
+ * @param {number} rootID 元素id
+ * @return {string} 返回html
+ */
+ReactDOMTextComponent.prototype.mountComponent = function(rootID) {
+  this._rootNodeID = rootID;
+  return `<span data-reactid="${this._rootNodeID}">${
+    this._currentElement
+  }</span>`;
+};
+
 export default ReactDOMTextComponent;
--- react/index.js
+++ react/index.js
@@ -1,3 +1,8 @@
 import React from "./React";
 
+var Com = "hello world!";
+var root = document.getElementById("container");
+
+React.render(Com, root);
+
 export default React;
