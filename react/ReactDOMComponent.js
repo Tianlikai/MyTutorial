7dac3e460ff994c941b718c68bd0ba8f9591be32
Tianlikai <1024998526@qq.com>
2018-10-22 15:32:09 +0800

    更新组件属性

--- react/ReactDOMComponent.js
+++ react/ReactDOMComponent.js
@@ -68,4 +68,80 @@ ReactDOMComponent.prototype.mountComponent = function(rootID) {
   return tagOpen + ">" + content + tagClose;
 };
 
+/**
+ * component 类 更新
+ * @param {*} nextElement
+ */
+ReactDOMComponent.prototype.receiveComponent = function(nextElement) {
+  var lastProps = this._currentElement.props;
+  var nextProps = nextElement.props;
+  this._currentElement = nextElement;
+  // 处理当前节点的属性
+  this._updateDOMProperties(lastProps, nextProps);
+  // 处理当前节点的子节点变动
+  this._updateDOMChildren(nextElement.props.children);
+};
+
+/**
+ *
+ * @param {*} lastProps
+ * @param {*} nextProps
+ */
+ReactDOMComponent.prototype._updateDOMProperties = function(
+  lastProps,
+  nextProps
+) {
+  // 当老属性不在新属性的集合里时，需要删除属性
+  var propName;
+  for (propName in lastProps) {
+    if (
+      nextProps.hasOwnProperty(propName) &&
+      !lastProps.hasOwnProperty(propName)
+    ) {
+      // 新属性中有，且不再老属性的原型中
+      continue;
+    }
+    if (/^on[A-Za-z]/.test(propName)) {
+      var eventType = propName.replace("on", "");
+      // 特殊事件，需要去掉事件监听
+      $(document).undelegate(
+        `[data-reactid="${this._rootNodeID}"]`,
+        eventType,
+        lastProps[propName]
+      );
+      continue;
+    }
+    // 删除不需要的属性
+    $(`[data-reactid="${this._rootNodeID}"]`).removeAttr(propName);
+  }
+  // 对于新的事件，需要写到dom上
+  for (propName in nextProps) {
+    if (propName === "children") continue;
+
+    if (/^on[A-Za-z]/.test(propName)) {
+      var eventType = propName.replace("on", "");
+      // 删除老的事件绑定
+      lastProps[propName] &&
+        $(document).undelegate(
+          `[data-reactid="${this._rootNodeID}"]`,
+          eventType,
+          lastProps[propName]
+        );
+      // 重新绑定
+      $(document).delegate(
+        `[data-reactid="${this._rootNodeID}"]`,
+        eventType,
+        nextProps[propName]
+      );
+      continue;
+    }
+
+    // 添加新的属性，重写同名属性
+    $(`[data-reactid="${this._rootNodeID}"]`).prop(
+      propName,
+      nextProps[propName]
+    );
+  }
+};
+
 export default ReactDOMComponent;
