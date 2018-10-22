63b51b52f6a21cb54f51628a2a3240d594890194
Tianlikai <1024998526@qq.com>
2018-10-22 17:27:03 +0800

    添加工具方法generateComponentChildren

--- react/util/flattenChildren.js
+++ react/util/flattenChildren.js
@@ -1,12 +1,14 @@
 /**
  * 将数组转换为映射
  * @param {Array} componentChildren
+ * @return {object} 返回一个映射
  */
 function flattenChildren(componentChildren) {
   var name;
   var childMap = {};
   componentChildren &&
     componentChildren.forEach((child, i) => {
+      // 一个组件下的子节点没有给定key值，则去下标作为标识
       name =
         child && child._currentElement && child._currentElement.key
           ? child._currentElement.key
