5fda6bc1c05e308f7ad25dbac853109bce78a2b9
Tianlikai <1024998526@qq.com>
2019-01-14 12:05:39 +0800

    update

--- ES6/promise/Promise.js
+++ ES6/promise/Promise.js
@@ -2,45 +2,42 @@ const PENDING = "pending";
 const FULFILLED = "fulfilled";
 const REJECTED = "rejected";
 
-/**
- * 实现 promisesaplus.com 规范
- * promise 状态只能为 pending fulfilled rejected
- * promise 状态一旦修改就不能再改变
- * @param {*} executor 执行器
- */
 function MyPromise(executor) {
   const self = this;
-  self.status = PENDING; // 设置状态
+  self.status = PENDING;
 
-  // success 回调数组
-  self.onResolvedCallbacks = [];
-
-  // fail 回调数组
+  self.onResolveCallbacks = [];
   self.onRejectCallbacks = [];
 
   function resolve(value) {
-    if (self.status === PENDING) {
-      self.status = FULFILLED;
-      self.value = value; // 该值不能修改
-      // 调用所有回调
-      self.onResolvedCallbacks.forEach(cb => cb(self.value));
+    if (value instanceof MyPromise) {
+      return value.then(resolve, reject);
     }
+
+    setTimeout(function() {
+      if (self.status === PENDING) {
+        self.status = FULFILLED;
+        self.value = value;
+        self.onResolveCallbacks.foreach(function(cb) {
+          cb(self.value);
+        });
+      }
+    });
   }
 
   function reject(reason) {
-    if (self.status === PENDING) {
-      self.status = REJECTED;
-      self.value = reason; // 该值不能修改
-      // 调用所有回调
-      self.onRejectCallbacks.forEach(cb => cb(self.value));
-    }
+    setTimeout(function() {
+      if (self.status === PENDING) {
+        self.status = REJECTED;
+        self.value = reason;
+        self.onRejectCallbacks.foreach(function(cb) {
+          cb(self.value);
+        });
+      }
+    });
   }
 
   try {
-    /**
-     * 传入的执行器可能回执行失败
-     * 捕获直接reject
-     */
     executor(resolve, reject);
   } catch (error) {
     reject(error);
@@ -48,55 +45,47 @@ function MyPromise(executor) {
 }
 
 function resolvePromise(promise2, x, resolve, reject) {
-  if (promise2 === x) {
-    return new TypeError("循环引用");
-  }
+  if (promise2 === x) return reject(new TypeError("循环引用"));
 
+  let called = false;
   if (x instanceof MyPromise) {
     if (x.status === PENDING) {
-      // 等待的话，值继续往下传递
       x.then(function(y) {
         resolvePromise(promise2, y, resolve, reject);
       }, reject);
     } else {
       x.then(resolve, reject);
     }
-  } else if (x !== null && (typeof x === "function" || typeof x === "object")) {
-    // x 是一个 thenable
+  } else if (x !== null && (typeof x === "object" || typeof x === "function")) {
     try {
       let then = x.then;
       if (typeof then === "function") {
-        // promise 相互之间的交互
-        // 自己写的promise 和 别人写的promise
-        // 考虑兼容性
         then.call(
-          this,
+          x,
           function(y) {
+            if (called) return;
+            called = true;
             resolvePromise(promise2, y, resolve, reject);
           },
-          function(error) {
-            reject(error);
+          function(reason) {
+            if (called) return;
+            called = true;
+            reject(reason);
           }
         );
       } else {
-        // x 不是一个 thenable 对象 resolve promise2
-        // 当作值resolve
         resolve(x);
       }
     } catch (error) {
-      reject(error);
+      if (called) return;
+      called = true;
+      resolve(x);
     }
   } else {
-    // x 是一个普通值，则用 x 的值 resolve promise2
     resolve(x);
   }
 }
 
-/**
- * 接受成功的值，或者失败的原因
- * @param {*} onFulfilled
- * @param {*} onRejected
- */
 MyPromise.prototype.then = function(onFulfilled, onRejected) {
   onFulfilled =
     typeof onFulfilled === "function" ? onFulfilled : value => value;
@@ -111,46 +100,65 @@ MyPromise.prototype.then = function(onFulfilled, onRejected) {
   let self = this;
   let promise2;
 
-  // success
   if (self.status === FULFILLED) {
     return (promise2 = new MyPromise(function(resolve, reject) {
-      try {
-        let x = onFulfilled(self.value);
-        // 走promise 解析过程
-        resolvePromise(promise2, x, resolve, reject);
-      } catch (error) {
-        reject(error);
-      }
+      setTimeout(function() {
+        try {
+          let x = onFulfilled(self.value);
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
     }));
   }
 
-  // failed
   if (self.status === REJECTED) {
-    console.log("REJECTED", self.value);
-    try {
-      let x = onRejected(self.value);
-      resolvePromise(promise2, x, resolve, reject);
-    } catch (error) {
-      reject(error);
-    }
+    return (promise2 = new MyPromise(function(resolve, reject) {
+      setTimeout(function() {
+        try {
+          let x = onRejected(self.value);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }));
   }
 
-  /**
-   * 还处于 pending 状态
-   * 把将来要执行的 success 或者 fail 函数
-   * 注册到 MyPromise 的 onResolvedCallbacks 和 onRejectCallbacks 数组中
-   */
   if (self.status === PENDING) {
-    self.onResolvedCallbacks.push(function() {
-      let x = onFulfilled(self.value);
-      resolvePromise(promise2, x, resolve, reject);
-    });
+    return (promise2 = new MyPromise(function(resolve, reject) {
+      self.onResolveCallbacks.push(function() {
+        try {
+          let x = onFulfilled(self.value);
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
 
-    self.onRejectCallbacks.push(function() {
-      let x = onRejected(self.value);
-      resolvePromise(promise2, x, resolve, reject);
-    });
+      self.onRejectCallbacks.push(function() {
+        try {
+          let x = onRejected(self.value);
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }));
   }
+};
+
+MyPromise.prototype.catch = function(onRejected) {
+  this.then(null, onRejected);
+};
+
+MyPromise.deferred = MyPromise.defer = function() {
+  let defer = {}; // 延迟对象
+  defer.promise = new MyPromise(function(resolve, reject) {
+    defer.resolve = resolve;
+    defer.reject = reject;
+  });
+  return defer;
 };
 
 module.exports = MyPromise;
