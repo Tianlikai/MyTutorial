3b73d9787d1b45b89838dc68527806b915e5fb4f
Tianlikai <1024998526@qq.com>
2019-03-03 17:45:08 +0800

    解决 leetcode 反转链表2

--- DataStructure/LinkedList/src/question.js
+++ DataStructure/LinkedList/src/question.js
@@ -1,3 +1,5 @@
+const ListNode = require("./Node");
+
 /**
  * leetcode 链表类题目
  */
@@ -25,7 +27,42 @@ class QLinkedList {
    * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
    * 输出: 1->4->3->2->5->NULL
    */
-  reverseBetween(head, m, n) {}
+  reverseBetween(head, m, n) {
+    if (m == n) return head;
+    let root = new ListNode(0);
+    root.next = head;
+    let i = 0,
+      next,
+      current = root,
+      beforeReversed = null, // 反转链表的前一项
+      previous = null, // 反转链表的第一项
+      endReversed = null; // 被反转链表段的最后一项
+
+    while (i < m - 1) {
+      current = current.next;
+      i += 1;
+    }
+
+    beforeReversed = current;
+    endReversed = current.next; // 此时 current.next 为反转链表的第一项，反转后为发转链表的最后一项
+
+    current = current.next;
+    i += 1;
+
+    while (i <= n) {
+      // 反转逻辑
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+      i += 1;
+    }
+
+    beforeReversed.next = previous;
+    endReversed.next = current;
+
+    return root.next;
+  }
 
   /**
    * 成对交换节点
