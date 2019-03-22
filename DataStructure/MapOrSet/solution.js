10698e43ddbc95f3c416ecd1d11edaa505783f5b
Tianlikai <1024998526@qq.com>
2019-03-22 18:16:26 +0800

    add leetcode 15. 三数之和

--- DataStructure/MapOrSet/solution.js
+++ DataStructure/MapOrSet/solution.js
@@ -95,8 +95,44 @@ class Solution {
     Object.keys(result).map(key => res.unshift(result[key]));
     return res;
   }
+
+  threeSum2(nums) {
+    let l, r, target, result;
+    result = [];
+    nums = nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i += 1) {
+      target = -nums[i];
+      l = i + 1;
+      r = nums.length - 1;
+      while (l < r) {
+        if (nums[l] + nums[r] > target) {
+          r -= 1;
+        } else if (nums[l] + nums[r] < target) {
+          l += 1;
+        } else {
+          result.push([-target, nums[l], nums[r]]);
+          while (l < r && nums[l] === nums[l + 1]) {
+            // 加速， 去重复
+            l += 1;
+          }
+          while (l < r && nums[r] === nums[r - 1]) {
+            // 加速， 去重复
+            r -= 1;
+          }
+          l += 1;
+          r -= 1;
+        }
+      }
+      // 加速， 去重复
+      while (i < nums.length - 2 && nums[i] === nums[i + 1]) {
+        i += 1;
+      }
+    }
+    return result;
+  }
 }
 var s = new Solution();
 
-const res = s.threeSum([-1, 1, 0]);
+const res = s.threeSum2([-1, 0, 1, 2, -1, -4]);
 console.log(res);
