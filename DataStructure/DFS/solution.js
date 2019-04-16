25fcfa5b76d778188ee1c213bfc0bc88814140e6
Tianlikai <1024998526@qq.com>
2019-04-16 21:48:47 +0800

    update

--- DataStructure/DFS/solution.js
+++ DataStructure/DFS/solution.js
@@ -5,14 +5,54 @@ class Solution {
    * @param {*} n
    */
   solveNQueens(n) {
+    if (n <= 1) return [];
     const result = [];
     const cols = new Set();
     const negativeX = new Set();
     const positiveX = new Set();
-
     nQueensDfs(n, 0, [], result, cols, negativeX, positiveX);
     return result;
   }
 }
 
-function nQueensDfs(n, row, curState, result, negativeX, positiveX) {}
+/**
+ * 深度优先
+ *
+ * @param {*} n n x n 棋盘
+ * @param {*} row 行
+ * @param {*} curState
+ * @param {*} result
+ * @param {*} cols 列集合
+ * @param {*} negativeX y = -x 集合
+ * @param {*} positiveX y = x 集合
+ */
+function nQueensDfs(n, y, curState, result, cols, negativeX, positiveX) {
+  if (y >= n) {
+    return result.push(curState);
+  }
+
+  for (let x = 0; x < n; x += 1) {
+    if (cols.has(x) || negativeX.has(x + y) || positiveX.has(y - x)) {
+      continue;
+    }
+
+    cols.add(x);
+    negativeX.add(x + y);
+    positiveX.add(y - x);
+
+    curState.push(x);
+    nQueensDfs(n, y + 1, curState, result, cols, negativeX, positiveX);
+
+    cols.clear();
+    negativeX.clear();
+    positiveX.clear();
+  }
+}
+
+const ins = new Solution();
+const result = ins.solveNQueens(4);
+
+for (let i = 0; i < result.length; i += 1) {
+  debugger;
+  console.log(result[i]);
+}
