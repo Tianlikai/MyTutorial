14e8eb2c29d813d623762b0dd4dc6565f04f91b4
Tianlikai <1024998526@qq.com>
2019-04-18 18:22:15 +0800

    add leetcode 152. 乘积最大子序列

--- DataStructure/DP/solution.js
+++ DataStructure/DP/solution.js
@@ -1,3 +1,49 @@
+/**
+ *
+ * @param {*} i 深度
+ * @param {*} j 广度
+ * @param {*} sum 和
+ * @param {*} array 原二维数组
+ * @param {*} result sum 数组
+ */
+function minimumTotalLoop(i, j, sum, array, result, depth) {
+  if (i >= depth) {
+    return result.push(sum);
+  }
+  let width = array[i].length;
+  for (let k = 0; k < width; k += 1) {
+    minimumTotalLoop(i + 1, k, sum + array[i][k], array, result, depth);
+    minimumTotalLoop(i + 1, k + 1, sum + array[i][k + 1], array, result, depth);
+  }
+}
+
+function climbStairsLoop(n, memo) {
+  if (n < 2) return 1;
+  if (memo[n]) return memo[n];
+  memo[n] = climbStairsLoop(n - 1, memo) + climbStairsLoop(n - 2, memo);
+  return memo[n];
+}
+
+/**
+ *
+ * @param {number} i 下标
+ * @param {array} nums 原数组
+ * @param {*} tempLMax 从左起，累计乘积
+ * @param {*} tempRMax 从右起，累计乘积
+ * @param {*} max 最大值
+ */
+function loopMaxProduct(i, nums, tempLMax, tempRMax, max) {
+  if (i >= nums.length) {
+    return max;
+  }
+  tempLMax *= nums[i];
+  tempRMax *= nums[nums.length - i - 1];
+  max = Math.max(max, Math.max(tempLMax, tempRMax));
+  if (tempLMax === 0) tempLMax = 1;
+  if (tempRMax === 0) tempRMax = 1;
+  return loopMaxProduct(i + 1, nums, tempLMax, tempRMax, max);
+}
+
 class Solution {
   /**
    * 70. 爬楼梯
@@ -65,35 +111,78 @@ class Solution {
     }
     return triangle[0][0];
   }
-}
 
-/**
- *
- * @param {*} i 深度
- * @param {*} j 广度
- * @param {*} sum 和
- * @param {*} array 原二维数组
- * @param {*} result sum 数组
- */
-function minimumTotalLoop(i, j, sum, array, result, depth) {
-  if (i >= depth) {
-    return result.push(sum);
+  /**
+   * leetcode 152. 乘积最大子序列
+   *
+   * 循环
+   * @param {number[]} nums
+   * @return {number}
+   */
+  maxProduct(nums) {
+    let max = nums[0];
+    let left = 1;
+    let right = 1;
+    for (let i = 0; i < nums.length; i += 1) {
+      left *= nums[i];
+      right *= nums[nums.length - i - 1];
+      max = Math.max(max, left, right);
+      if (left == 0) left = 1;
+      if (right == 0) right = 1;
+    }
+    return max;
   }
-  let width = array[i].length;
-  for (let k = 0; k < width; k += 1) {
-    minimumTotalLoop(i + 1, k, sum + array[i][k], array, result, depth);
-    minimumTotalLoop(i + 1, k + 1, sum + array[i][k + 1], array, result, depth);
+
+  /**
+   * leetcode 152. 乘积最大子序列
+   *
+   * 递归
+   * @param {number[]} nums
+   * @return {number}
+   */
+  maxProduct2(nums) {
+    if (nums.length === 1) return nums[0];
+    return loopMaxProduct(0, nums, 1, 1, nums[0]);
+  }
+
+  /**
+   * leetcode 152. 乘积最大子序列
+   *
+   * 动态规划
+   * @param {number[]} nums
+   * @return {number}
+   */
+  maxProduct3(nums) {
+    if (!nums) return 0;
+    const dp = [];
+    dp[0] = [];
+    dp[0][0] = nums[0];
+    dp[0][1] = nums[0];
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i += 1) {
+      if (!dp[i]) dp[i] = [];
+
+      dp[i][0] = Math.max(
+        dp[i - 1][0] * nums[i],
+        dp[i - 1][1] * nums[i],
+        nums[i]
+      );
+
+      dp[i][1] = Math.min(
+        dp[i - 1][0] * nums[i],
+        dp[i - 1][1] * nums[i],
+        nums[i]
+      );
+
+      max = Math.max(max, dp[i][0]);
+    }
+    return max;
   }
 }
 
-function climbStairsLoop(n, memo) {
-  if (n < 2) return 1;
-  if (memo[n]) return memo[n];
-  memo[n] = climbStairsLoop(n - 1, memo) + climbStairsLoop(n - 2, memo);
-  return memo[n];
-}
-
-const array = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]];
+const array = [2, -5, -2, -4, 3];
 
 const ins = new Solution();
-const result = ins.minimumTotal2(array);
+const result = ins.maxProduct3(array);
+console.log(result);
