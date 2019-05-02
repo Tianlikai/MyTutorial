53ad506d01a525b68fc443cdfa7da89fc0d52e43
Tianlikai <1024998526@qq.com>
2019-05-02 16:32:26 +0800

    用函数模拟new函数

---
+++ ES6/myNew.js
@@ -0,0 +1,13 @@
+/**
+ * 模拟new关键字
+ * 创建一个新对象
+ * 将新对象的__proto__指向构造函数的prototype
+ * 执行构造函数，如果构造函数的返回值为object，则返回返回值，反之返回新的对象
+ */
+function myNew() {
+  var obj = new Object();
+  var Constructor = [].shift.call(arguments);
+  obj.__proto__ = Constructor.prototype;
+  var res = Constructor.apply(obj, arguments);
+  return typeof res === "object" ? res : obj;
+}
