72f28bf57b4cf3e6a4e657132a5d7b6c11abfd74
Tianlikai <1024998526@qq.com>
2019-05-02 16:31:42 +0800

    一个比较好的类型验证函数

---
+++ ES6/type.js
@@ -0,0 +1,17 @@
+const typeFactory = () => {
+  const class2type = {};
+  "Array Date RegExp Object Error"
+    .split(" ")
+    .forEach(e => (class2type["[object " + e + "]"] = e.toLowerCase()));
+
+  return function type(obj) {
+    if (obj === null) return String(obj);
+    return typeof obj === "object"
+      ? class2type[Object.prototype.toString.call(obj)] || "object"
+      : typeof obj;
+  };
+};
+
+const type = typeFactory();
+
+module.exports = type;
